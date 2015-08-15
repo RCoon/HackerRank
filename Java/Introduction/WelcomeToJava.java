b01592f5f941d0ae08e3968ad0c3eb8a85ab8a0d
Ryan Coon <rybread354@aol.com>
2015-08-15 09:47:07 -0500

    Add WelcomeToJava

---
+++ Java/Introduction/WelcomeToJava.java
@@ -0,0 +1,12 @@
+/*
+ * Welcome to the world of Java! Just print "Hello World." and "Hello Java."
+ * in two separate lines to complete this challenge.
+ */
+ 
+public class WelcomeToJava {
+     
+	public static void main(String[] args) {
+	    System.out.println("Hello World.");
+        System.out.println("Hello Java.");
+	}
+}
