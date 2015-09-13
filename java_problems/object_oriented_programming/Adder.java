ff6c693aff058be76a4cd13a217852241ee8f540
Ryan Coon <rybread354@aol.com>
2015-09-13 08:10:48 -0500

    Add Adder

---
+++ java_problems/object_oriented_programming/Adder.java
@@ -0,0 +1,21 @@
+package java_problems.object_oriented_programming;
+
+public class Adder {
+
+	private int x,y,sum;
+    
+    public Adder() {
+        
+    }
+    
+    public int add(int augend, int addend) {
+        x = augend;
+        y = addend;
+        sum = augend + addend;
+        return sum;
+    }
+}
+
+class Arithmetic {
+    
+}
