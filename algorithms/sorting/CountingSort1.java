1b536ff90607915990e6917cdb906ccce15625d8
Ryan Coon <rybread354@aol.com>
2015-12-09 09:24:59 -0600

    Add CountingSort2, update CountingSort1

--- algorithms/sorting/CountingSort1.java
+++ algorithms/sorting/CountingSort1.java
@@ -36,7 +36,7 @@ import java.util.Scanner;
  */
 public class CountingSort1 {
 
-public static void main(String[] args) {
+	public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
         int listSize = sc.nextInt();
