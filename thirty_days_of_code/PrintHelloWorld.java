6ea31eeffa42578c805c21b79cca8958bf303ea4
Ryan Coon <rybread354@aol.com>
2015-12-31 08:00:09 -0600

    Add PrintHelloWorld

---
+++ thirty_days_of_code/PrintHelloWorld.java
@@ -0,0 +1,16 @@
+package thirty_days_of_code;
+
+/*
+ * For this challenge, all you have to do is print the following two lines:
+ * 
+ * Hello World.
+ * Welcome to 30 Days of Code.
+ */
+public class PrintHelloWorld {
+
+	public static void main(String[] args) {
+        
+        System.out.println("Hello World.");
+        System.out.println("Welcome to 30 Days of Code.");
+    }
+}
