e12a05664ea7734f8e77f98df5285de77a07c28c
Ryan Coon <rybread354@aol.com>
2016-02-10 10:11:15 -0600

    Add DisplayInfoFromObject

---
+++ seven_days_of_javascript/DisplayInfoFromObject.js
@@ -0,0 +1,45 @@
+/*
+Task
+
+Write a JavaScript program to display the status (i.e. display book name, author name and reading status) of books. You are given an object library in the code's template. It contains a list of books with the above mentioned properties. Your task is to display the following:
+
+If the book is unread:
+
+You still need to read '<book_name>' by <author_name>.
+If the book is read:
+
+Already read '<book_name>' by <author_name>.
+*/
+
+function displayInformation() {
+     // var library is defined, use it to print the information
+    for (var i = 0; i < library.length; i++) {
+        var book = library[i];
+        if (book.readingStatus) {
+            console.log("Already read '" + book.title + "' by " + book.author + ".");
+        } else {
+            console.log("You still need to read '" + book.title + "' by " + book.author + ".");
+        }
+    }
+} 
+
+// tail starts here
+var library = [ 
+    {
+        title: 'Bill Gates',
+        author: 'The Road Ahead',
+        readingStatus: true
+    },
+    {
+        title: 'Steve Jobs',
+        author: 'Walter Isaacson',
+        readingStatus: true
+    },
+    {
+        title: 'Mockingjay: The Final Book of The Hunger Games',
+        author: 'Suzanne Collins',
+        readingStatus: false
+    }
+];
+
+displayInformation();
