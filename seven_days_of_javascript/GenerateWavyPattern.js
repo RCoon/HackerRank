c30742afdd9f8055f1d001d2c611a2f253fecfa1
Ryan Coon <rybread354@aol.com>
2016-02-14 15:08:12 -0600

    Add Day 7 files

---
+++ seven_days_of_javascript/GenerateWavyPattern.js
@@ -0,0 +1,6 @@
+function processData(input) {
+
+    const backSlash = String.fromCharCode(9585);
+    const forwardSlash = String.fromCharCode(9586);
+    console.log(((backSlash + forwardSlash).repeat(7) + '\n').repeat(14));
+} 
