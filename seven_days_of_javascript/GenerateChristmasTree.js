82594cde3fee3d2ab151c3659c3f2615494050e6
Ryan Coon <rybread354@aol.com>
2016-02-14 17:19:25 -0600

    Add GenerateChristmasTree

---
+++ seven_days_of_javascript/GenerateChristmasTree.js
@@ -0,0 +1,34 @@
+/*
+Write a JavaScript program to generate the Christmas Tree pattern below.
+The tree should be composed of zeroes (0), and it must be topped with an asterisk (*).
+
+        *
+        0
+       000
+      00000
+     0000000
+    000000000
+   00000000000
+  0000000000000
+ 000000000000000
+00000000000000000
+Note: The leftmost 0 should be aligned with the left edge and there should not be any
+blank spaces preceding it on the last line.
+
+Input Format:
+No input is required to complete this challenge.
+*/
+function processData(input) {
+   var loveTree = `        *
+        0
+       000
+      00000
+     0000000
+    000000000
+   00000000000
+  0000000000000
+ 000000000000000
+00000000000000000
+`
+    console.log(loveTree);  
+}
